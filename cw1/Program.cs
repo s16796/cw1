f7098fa26bac74d187a99b45a7f7fd6eef39d2bb
Tomasz Rachański <s16796@pjwstk.edu.pl>
2020-03-01 12:19:48 +0100

    Removed commented out code

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -10,7 +10,6 @@ namespace cw1
         static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            //var response = await httpClient.GetAsync(args[0]);
             var doc = await httpClient.GetStringAsync(args[0]);
             Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
             MatchCollection collection = emailRegex.Matches(doc);
@@ -18,8 +17,6 @@ namespace cw1
             {
                 Console.WriteLine(match);
             }
-            //Console.WriteLine(response);
-            //Console.WriteLine(doc);
         }
     }
 }
