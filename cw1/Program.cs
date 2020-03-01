0f589cbd902d9e2ebdd959de41ddb20682988290
Tomasz Rachański <s16796@pjwstk.edu.pl>
2020-03-01 12:13:08 +0100

    Pierwszy commit

---
+++ cw1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            //var response = await httpClient.GetAsync(args[0]);
+            var doc = await httpClient.GetStringAsync(args[0]);
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+            MatchCollection collection = emailRegex.Matches(doc);
+            foreach(Match match in collection)
+            {
+                Console.WriteLine(match);
+            }
+            //Console.WriteLine(response);
+            //Console.WriteLine(doc);
+        }
+    }
+}
