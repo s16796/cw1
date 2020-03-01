408e3558fcf7fa9c8084d679344077bf98f9e1d4
Tomasz Rachański <s16796@pjwstk.edu.pl>
2020-03-01 12:53:04 +0100

    Finalna wersja z zajec

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -5,18 +5,38 @@ using System.Threading.Tasks;
 
 namespace cw1
 {
-    class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            var doc = await httpClient.GetStringAsync(args[0]);
+            HttpResponseMessage res = await httpClient.GetAsync("https://www.pja.edu.pl/");
+
+            if (res.IsSuccessStatusCode)
+            {
+                string html = await res.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+
+            }
+
+            Console.WriteLine("Koniec!");
+
+
+            //Wlasne rozwiązanie napisane przed omówieniem na lekcji
+            /*
+            string doc = await httpClient.GetStringAsync(args[0]); //"https://www.pja.edu.pl/" przekazane w argumencie
             Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
             MatchCollection collection = emailRegex.Matches(doc);
             foreach(Match match in collection)
             {
                 Console.WriteLine(match);
             }
+            */
         }
     }
 }
